711a01190416d0e54d5656cc364776b179906f5e
Ruins7 <704657512@qq.com>
2017-02-16 22:05:33 -0500

    update

---
+++ src/easy/Sqrt.java
@@ -0,0 +1,32 @@
+/**  
+ * @Title: Sqrt.java
+ * @Prject: Algorithms
+ * @Package: easy
+ * @Description: TODO
+ * @author: ruins7  
+ * @date: Feb 16, 2017 10:00:21 PM
+ * @version: V1.0  
+ */
+package easy;
+
+/**
+ * @ClassName: Sqrt
+ * @Description: TODO
+ * @author: ruins7
+ * @date: Feb 16, 2017 10:00:21 PM
+ */
+public class Sqrt {
+
+	/**
+	 * @Title: main
+	 * @Description: lintCode 141
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		System.out.println((int) Math.sqrt(x));
+	}
+
+}
