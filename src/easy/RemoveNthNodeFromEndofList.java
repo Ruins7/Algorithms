6a7d50b1350a3e23f915621dde3df9f64f9b2630
Ruins7 <704657512@qq.com>
2017-02-19 14:08:27 -0500

    update

---
+++ src/easy/RemoveNthNodeFromEndofList.java
@@ -0,0 +1,44 @@
+/**  
+ * @Title: RemoveNthNodeFromEndofList.java
+ * @Prject: Algorithms
+ * @Package: easy
+ * @Description: TODO
+ * @author: ruins7  
+ * @date: Feb 19, 2017 1:43:29 PM
+ * @version: V1.0  
+ */
+package easy;
+
+/**
+ * @ClassName: RemoveNthNodeFromEndofList
+ * @Description: TODO
+ * @author: ruins7
+ * @date: Feb 19, 2017 1:43:29 PM
+ */
+public class RemoveNthNodeFromEndofList {
+
+	/**
+	 * @Title: main
+	 * @Description: LintCode 174
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		ListNode n1 = new ListNode(1);
+//		ListNode n2 = new ListNode(2);
+//		ListNode n3 = new ListNode(3);
+//		ListNode n4 = new ListNode(4);
+//		ListNode n5 = new ListNode(5);
+		
+		 
+		 
+		 
+//		int newindex = list.size() - n;
+//		list.remove(newindex);
+
+	}
+
+	
+
+}
