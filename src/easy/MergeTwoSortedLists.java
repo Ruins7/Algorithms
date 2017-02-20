c97adcc8771f01bb8208165f563ec02da71dd14e
Ruins7 <704657512@qq.com>
2017-02-20 15:46:57 -0500

    update

--- src/easy/MergeTwoSortedLists.java
+++ src/easy/MergeTwoSortedLists.java
@@ -9,6 +9,10 @@
  */
 package easy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import Tools.ListNode;
 
 /**
@@ -41,17 +45,48 @@ public class MergeTwoSortedLists {
 		ListNode m2 = new ListNode(6);
 		l2.next = m2;
 		
-		ListNode node1 = l1;
-		ListNode node2 = l2;
 		
-		while(node1.next != null){
-			while(node2.next != null){
-				
+		ListNode pre_node = null;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+
+		if(l1 != null){
+			while(l1.next != null || l1.val != 0){
+				pre_node = l1.next;
+				list.add(l1.val);
+				if (pre_node == null) {//如果当前元素没有下一个元素，则跳出循环
+					break;
+				} else {//如果有，则把当前元素赋给下一次循环的node
+					l1 = pre_node;
+				}
 			}
 		}
+		if(l2 != null){
+			while(l2.next != null || l2.val != 0){
+				pre_node = l2.next;
+				list.add(l2.val);
+				if (pre_node == null) {//如果当前元素没有下一个元素，则跳出循环
+					break;
+				} else {//如果有，则把当前元素赋给下一次循环的node
+					l2 = pre_node;
+				}
+			}
+		}
+		if(list == null || list.isEmpty()){
+			//return null;
+		}
 		
+		Collections.sort(list);
+		List<ListNode> nodelist = new ArrayList<ListNode>();
 		
-		
+		for (int i = 0; i < list.size(); i++) {
+			nodelist.add(new ListNode(list.get(i)));
+		}
+		ListNode head = nodelist.get(0);
+		for (int i = 0; i < nodelist.size(); i++) {
+			if(i+1 < nodelist.size()){
+				nodelist.get(i).next = nodelist.get(i+1);//重点！！
+			}
+		}
 		
 		
 		
