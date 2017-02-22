43896e7151978018bf69567423d78a27db713ca1
Ruins7 <704657512@qq.com>
2017-02-22 13:04:45 -0500

    update

---
+++ src/easy/NthLastNodeinList.java
@@ -0,0 +1,31 @@
+/**  
+ * @Title: NthLastNodeinList.java
+ * @Prject: Algorithms
+ * @Package: easy
+ * @Description: TODO
+ * @author: ruins7  
+ * @date: Feb 22, 2017 1:03:47 PM
+ * @version: V1.0  
+ */
+package easy;
+
+/**
+ * @ClassName: NthLastNodeinList
+ * @Description: TODO
+ * @author: ruins7
+ * @date: Feb 22, 2017 1:03:47 PM
+ */
+public class NthLastNodeinList {
+
+	/**
+	 * @Title: main
+	 * @Description: TODO
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
