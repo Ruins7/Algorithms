a3c4154622398e24c0a499f3ba7975b67392308a
Ruins7 <704657512@qq.com>
2017-02-23 14:03:20 -0500

    update

---
+++ src/easy/InsertionSortList.java
@@ -0,0 +1,31 @@
+/**  
+ * @Title: InsertionSortList.java
+ * @Prject: Algorithms
+ * @Package: easy
+ * @Description: TODO
+ * @author: ruins7  
+ * @date: Feb 23, 2017 2:02:28 PM
+ * @version: V1.0  
+ */
+package easy;
+
+/**
+ * @ClassName: InsertionSortList
+ * @Description: TODO
+ * @author: ruins7
+ * @date: Feb 23, 2017 2:02:28 PM
+ */
+public class InsertionSortList {
+
+	/**
+	 * @Title: main
+	 * @Description: LintCode 173
+	 * @param args
+	 * @return: void
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
